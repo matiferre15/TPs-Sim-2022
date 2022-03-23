5d27af70c05736bb7927c1bad1da7f23326585df
@LucasDiaz <ldiaz@encodesa.com.ar>
2022-03-22 22:30:31 -0300

    primer commit

---
+++ Tp1-Sim/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Tp1_Sim
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Metodo = new System.Windows.Forms.GroupBox();
+            this.cmb_Metodo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSemilla = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtM = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_numSimulaciones = new System.Windows.Forms.TextBox();
+            this.Metodo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Metodo
+            // 
+            this.Metodo.Controls.Add(this.txt_numSimulaciones);
+            this.Metodo.Controls.Add(this.label6);
+            this.Metodo.Controls.Add(this.label5);
+            this.Metodo.Controls.Add(this.label4);
+            this.Metodo.Controls.Add(this.label3);
+            this.Metodo.Controls.Add(this.label2);
+            this.Metodo.Controls.Add(this.txtC);
+            this.Metodo.Controls.Add(this.txtM);
+            this.Metodo.Controls.Add(this.txtA);
+            this.Metodo.Controls.Add(this.txtSemilla);
+            this.Metodo.Controls.Add(this.label1);
+            this.Metodo.Controls.Add(this.cmb_Metodo);
+            this.Metodo.Location = new System.Drawing.Point(12, 12);
+            this.Metodo.Name = "Metodo";
+            this.Metodo.Size = new System.Drawing.Size(427, 169);
+            this.Metodo.TabIndex = 0;
+            this.Metodo.TabStop = false;
+            this.Metodo.Text = "Metodo";
+            // 
+            // cmb_Metodo
+            // 
+            this.cmb_Metodo.FormattingEnabled = true;
+            this.cmb_Metodo.Location = new System.Drawing.Point(72, 27);
+            this.cmb_Metodo.Name = "cmb_Metodo";
+            this.cmb_Metodo.Size = new System.Drawing.Size(121, 23);
+            this.cmb_Metodo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Metodo";
+            // 
+            // txtSemilla
+            // 
+            this.txtSemilla.Location = new System.Drawing.Point(308, 22);
+            this.txtSemilla.Name = "txtSemilla";
+            this.txtSemilla.Size = new System.Drawing.Size(100, 23);
+            this.txtSemilla.TabIndex = 2;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(308, 51);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 23);
+            this.txtA.TabIndex = 3;
+            // 
+            // txtM
+            // 
+            this.txtM.Location = new System.Drawing.Point(308, 80);
+            this.txtM.Name = "txtM";
+            this.txtM.Size = new System.Drawing.Size(100, 23);
+            this.txtM.TabIndex = 4;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(308, 109);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(100, 23);
+            this.txtC.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(248, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Semilla";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(267, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "m";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(267, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "a";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(267, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "c";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Nº de simulaciones";
+            // 
+            // txt_numSimulaciones
+            // 
+            this.txt_numSimulaciones.Location = new System.Drawing.Point(121, 80);
+            this.txt_numSimulaciones.Name = "txt_numSimulaciones";
+            this.txt_numSimulaciones.Size = new System.Drawing.Size(72, 23);
+            this.txt_numSimulaciones.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 556);
+            this.Controls.Add(this.Metodo);
+            this.Name = "Form1";
+            this.Text = "TP 1 - Grupo 1";
+            this.Metodo.ResumeLayout(false);
+            this.Metodo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Metodo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmb_Metodo;
+        private System.Windows.Forms.TextBox txt_numSimulaciones;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtM;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtSemilla;
+    }
+}
+
