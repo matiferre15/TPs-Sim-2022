6e9d6d03341e8de3d2b1fab3d3e1e76d9fe71210
matiferre15 <matias.ferre@hotmail.com>
2022-09-08 21:01:33 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ISW_TPE_6_4K2_G4';
+}
